1a04427eff5a6a54540a3edd38297680cfc8f326
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2019-07-14 20:02:32 +0200

    Conenct to heroku database

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
-const express = require('express');
-const expressjwt = require('express-jwt');
-const jwks = require('jwks-rsa');
-const app = express();
-const { Client } = require('pg');
-const bodyParser = require('body-parser');
-
-const router = require('./routes/public');
-
-const cool = require('cool-ascii-faces');
-const path = require('path');
+const express = require('express')
+// const expressjwt = require('express-jwt')
+// const jwks = require('jwks-rsa')
+const app = express()
+const bodyParser = require('body-parser')
+const cool = require('cool-ascii-faces')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.urlencoded({ extended: false }))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    // .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
-app.use(router);
-app.use(bodyParser.urlencoded({ extended: false }));
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+    user: 'dkokjeijzfquzw',
+    host: 'ec2-174-129-29-101.compute-1.amazonaws.com',
+    database: 'd8964drglas3td',
+    password: '77602e2682f2e28fa06d6d26599e2b16aba9c74c6da743a86d24284e8304eeec',
+    port: 5432,
+    ssl: true
+})
+
+client.connect()
+
+app.get('/connection', (req, res) => {
+    client.query('SELECT * FROM budget', (error, response) => {
+        console.log(error ? error.stack : 'connected to database');
+        res.send(response.rows)
+        client.end()
+    })
+})
+
+
+app.get('/public', (req, res) => {
+    res.status(200).send('Public access');
+})
+
+
+app.post('/add_transaction', (req, res) => {
+    client.query(
+        {
+            text: 'INSERT INTO budget(type,value) VALUES($1,$2)',
+            values: [req.body.create_type, req.body.create_value],
+            rowMode: 'array'
+        }, (error, response) => {
+            res.end()
+            client.end()
+        })
+    res.end()
+})
+
+
+
 
 // const authConfig = {
 //     domain: "miedzyslowami.eu.auth0.com",
@@ -40,57 +78,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //     algorithms: ['RS256']
 // });
 
-
-// app.get('/public', (req, res) => {
-//     res.status(200).send('Public access');
-// });
-
 // app.get('/private', jwtCheck, (req, res) => {
 //     res.status(200).send('Your Access Token was successfully validated!');
 // })
 
 
 
-const client = new Client({
-    user: 'dkokjeijzfquzw',
-    host: 'ec2-174-129-29-101.compute-1.amazonaws.com',
-    database: 'd8964drglas3td',
-    password: '77602e2682f2e28fa06d6d26599e2b16aba9c74c6da743a86d24284e8304eeec',
-    port: 5432,
-});
-
 //TODO refactor to connection pool
-client.connect();
 
-app.get('/connection', (req, res) => {
-    client.query('SELECT * FROM public.transactions', (error, response) => {
-        console.log(error ? error.stack : response.rows);
-        res.send(response.rows);
-        client.end();
-    })
+// pool.connect((err, client, done) => {
+//     if (err) throw err;
 
-})
+//     app.get('/connection', (req, res) => {
+//         client.query('SELECT * FROM public.transactions', (error, response) => {
+//             console.log(error ? error.stack : response.rows);
+//             res.send(response.rows);
+//             client.end();
+//         })
 
-app.post('/add_transaction', (req, res) => {
-    console.log('Adding new row to database:', req.body.create_type, req.body.create_value);
-    client.query(
-        {
-            text: "INSERT INTO public.transactions(type,value) VALUES($1,$2)",
-            values: [req.body.create_type,req.body.create_value],
-            rowMode: 'array'
-        }, (error, response) => {
-        console.log(error ? error.stack : response.rows);
-        res.end();
-        client.end()
-    });
-    res.end();
-})
+//     })
+//     // client.query('SELECT * FROM users WHERE id = $1', [1], (err, res) => {
+//     //   done()
 
-app.get('/connection/transaction/:id', (req, res) => {
-    
-    client.query('SELECT * FROM public.transactions', (error, response) => {
-        console.log(error ? error.stack : response.rows);
-        res.send(response.rows[req.params.id]);
-        client.end();
-    })
-})
+//     //   if (err) {
+//     //     console.log(err.stack)
+//     //   } else {
+//     //     console.log(res.rows[0])
+//     //   }
+//     // })
+
+//   })
+
+
+
+
+// app.get('/connection/transaction/:id', (req, res) => {
+
+//     client.query('SELECT * FROM public.transactions', (error, response) => {
+//         console.log(error ? error.stack : response.rows);
+//         res.send(response.rows[req.params.id]);
+//         client.end();
+//     })
+// })
--- routes/public.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/public', (req, res) => {
-    res.send('Welcome to public route');
-    res.end();
-});
-
-module.exports = router;
